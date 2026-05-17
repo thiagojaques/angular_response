796945a0b319c91583c65db7a5b8dfe6a53ed47a
thiagojaques <thiagohenriquedelimajaques@gmail.com>
2026-05-17 12:03:22 -0400

    Initial commit

---
+++ src/app/my-component/my-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyComponent } from './my-component';
+
+describe('MyComponent', () => {
+  let component: MyComponent;
+  let fixture: ComponentFixture<MyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MyComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/my-component/my-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  imports: [],
+  templateUrl: './my-component.html',
+  styleUrl: './my-component.css',
+})
+export class MyComponent {}
